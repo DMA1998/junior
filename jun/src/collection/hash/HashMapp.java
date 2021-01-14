65a4e8484cb8aa2e3960c5cd2e06ea244e55cfaf
Dmitriy Mykh <AX5555AC@gmail.com>
2021-01-14 15:03:58 +0200

    fixed

--- jun/src/collection/hash/HashMapp.java
+++ jun/src/collection/hash/HashMapp.java
@@ -2,42 +2,26 @@ package collection.hash;
 
 import collection.model.Employee;
 
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 public class HashMapp {
-/*
+
     public static void main(String[] args) {
       Map<String,Employee> map = new HashMap<>();
-      Employee dima = new Employee("Dima",22,"manager");
-      map.put("+380979327796",dima);
+      Employee robert = new Employee("Robert",35,"manager");
+      map.put("Robert's key",robert);
+
+      Employee liza = new Employee(null,0,null);
+      liza = map.get("Robert's key");
+      map.put("Liza's key",liza);
 
         for(Map.Entry<String,Employee> employee : map.entrySet()) {
-            System.out.println("Phone:" +  employee.getKey() + "\n" + employee.getValue());
+            System.out.println(employee.getKey() + "\n" + employee.getValue());
+            System.out.println();
         }
-    }*/
 
-    public static void main(String[] args) {
-        int[] array = {1, 6, 3, 7, 2, 1, 6, 3, 34, 12, 6, 1, 4213, 31212, 51, 5123, 3};
-
-        bubbleSort(array);
-
-        System.out.println(Arrays.toString(array));
-    }
-
-    static void bubbleSort(int[] array) {
-        int temp;
-        for (int i = 0; i < array.length - 1; i++) {
-            for (int j = 1; j < array.length - i; j++) {
-                if (array[j - 1] > array[j]) {
-                    temp = array[j - 1];
-                    array[j - 1] = array[j];
-                    array[j] = temp;
-
-                }
-            }
-        }
+        System.out.println("Employees into map: " + map.size());
     }
 }
 
